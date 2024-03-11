27083b1de6302924e43618cb972763e45cff8500
erlmatt <96199777+erlmatt@users.noreply.github.com>
2024-03-11 10:49:45 +0100

    anlgegen von port und host sowie implementierung der logik des programms

--- app/src/main/java/com/example/se2_einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.se2_einzelphase;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private EditText input;
+    private final static String host = "se2-submission.aau.at";
+    private final static int port = 20080;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+    //input = (EditText) findViewById(R.id.input);
+    //TextView output = (TextView) findViewById(R.id.output);
+
+    private int alternierendeQuersumme(String matrikelnummer){
+        int alternierendeQuersumme = 0;
+        for(int i = 0; i < matrikelnummer.length(); i++){
+            int ziffer = Integer.parseInt(String.valueOf(matrikelnummer.charAt(i)));
+            if(i % 2 == 0){
+                alternierendeQuersumme += ziffer;
+            }else {
+                alternierendeQuersumme -= ziffer;
+            }
+
+        }
+        return alternierendeQuersumme;
+    };
+
+    private boolean istGerade(int zahl){
+        if (zahl % 2 == 0) return true;
+        return false;
+    }
+
+
+
 }
